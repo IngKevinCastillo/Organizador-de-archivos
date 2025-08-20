f552179d89465791f079553243f62d3417d80cc3
IngKevinCastillo <161144431+IngKevinCastillo@users.noreply.github.com>
2025-08-20 08:39:52 -0500

    Add files via upload

---
+++ Organizador.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+
+extensions_dict = {
+    '.txt': 'documentos',
+    '.doc': 'documentos',
+    '.docx': 'documentos',
+    '.xls': 'documentos',
+    '.xlsx': 'documentos',
+    '.ppt': 'documentos',
+    '.pptx': 'documentos',
+    '.pdf': 'documentos',
+
+    '.jpg': 'imagenes',
+    '.jpeg': 'imagenes',
+    '.png': 'imagenes',
+    '.gif': 'imagenes',
+    '.bmp': 'imagenes',
+    '.ico': 'imagenes',
+    '.svg': 'imagenes',
+
+    '.mp4': 'videos',
+    '.mkv': 'videos',
+    '.avi': 'videos',
+    '.mov': 'videos',
+    '.wmv': 'videos',
+    '.flv': 'videos',
+
+    '.py': 'programacion',
+    '.java': 'programacion',
+    '.c': 'programacion',
+    '.cpp': 'programacion',
+    '.cs': 'programacion',
+    '.js': 'programacion',
+    '.ts': 'programacion',
+    '.html': 'programacion',
+    '.css': 'programacion',
+    '.php': 'programacion',
+
+    '.mcpack': 'minecraft',
+    '.mcaddon': 'minecraft',
+    '.mcworld': 'minecraft',
+
+    '.apk': 'apk',
+
+    '.exe': 'ejecutable',
+    '.msi': 'ejecutable'
+}
+
+predeterminada = 'otros'
+carpeta_organizar_ruta = r'C:\Users\kevin\Downloads\otros'
+
+archivos = os.listdir(carpeta_organizar_ruta)
+
+for archivo in archivos:
+    archivo_origen_ruta = os.path.join(carpeta_organizar_ruta, archivo)
+
+    if os.path.isfile(archivo_origen_ruta):
+        _, extension = os.path.splitext(archivo)
+        extension = extension.lower()
+
+        carpeta_destino = extensions_dict.get(extension, predeterminada)
+        carpeta_destino_ruta = os.path.join(carpeta_organizar_ruta, carpeta_destino)
+
+        if not os.path.exists(carpeta_destino_ruta):
+            os.makedirs(carpeta_destino_ruta)
+
+        shutil.move(archivo_origen_ruta, os.path.join(carpeta_destino_ruta, archivo))
+
+print("Archivos organizados correctamente.")
